8d3096cd8a656a593878bdd209d7759b2e44e7a0
Zach Baker <zach.r.baker1@gmail.com>
2015-10-22 16:54:04 -0600

    the text box will now countdown

--- animations.js
+++ animations.js
@@ -3,12 +3,27 @@ $(document).ready(function(){
 	$('#tweet-submit').hide();
 	$('#char-count').hide();
 	
-	$('.tweet-compose').on('focus', function(){
+	
+	
+	
+	
+	
+	$('.tweet-compose').on('click', function(){
 		$('#tweet-submit').show();
 		$('#char-count').show();
 		$('.tweet-compose').css('height', '5em');
+		
 	});
 	
+	
+	
+	
+	
+	$('#notReply').keydown(function() {
+			var userTweet = $('#notReply').val().length;
+			console.log(userTweet);
+			$('#char-count').text(140 - userTweet);
+	});
 	
 	
 });
