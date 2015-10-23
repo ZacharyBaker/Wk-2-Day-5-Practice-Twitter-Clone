a55226d9062a676f62c8332a483efee7e5f1a799
Zach Baker <zach.r.baker1@gmail.com>
2015-10-23 13:57:02 -0600

    on blur the box returns to normal state

--- animations.js
+++ animations.js
@@ -8,11 +8,17 @@ $(document).ready(function(){
 	
 	
 	
-	$('.tweet-compose').on('click', function(){
+	$('.tweet-compose').on('focus', function(){
 		$('#tweet-submit').show();
 		$('#char-count').show();
 		$('.tweet-compose').css('height', '5em');
 		
+	});
+	
+	$('.tweet-compose').on('blur', function(){
+		$('#tweet-submit').hide();
+		$('#char-count').hide();
+		$('.tweet-compose').css('height', '2.5em');
 	});
 	
 	
@@ -27,9 +33,13 @@ $(document).ready(function(){
 				//use a while loop for the userTweet length being 10 or less.
 			if (userTweet >= 130){
 				$('#char-count').css('color', 'red');
+			} else {
+				$('#char-count').css('color', 'black');
 			}
+			return userTweet;
+			
+		
 		} 
 	});
-	
 	
 });
