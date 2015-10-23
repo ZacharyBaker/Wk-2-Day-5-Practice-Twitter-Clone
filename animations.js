1ef43773a380de12da27c7fb1d2697eac84207f8
Zach Baker <zach.r.baker1@gmail.com>
2015-10-23 14:48:19 -0600

    disable box and text number

--- animations.js
+++ animations.js
@@ -1,45 +1,52 @@
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
 	$('#tweet-submit').hide();
 	$('#char-count').hide();
-	
-	
-	
-	
-	
-	
-	$('.tweet-compose').on('focus', function(){
+
+
+
+
+
+
+	$('.tweet-compose').on('focus', function () {
 		$('#tweet-submit').show();
 		$('#char-count').show();
 		$('.tweet-compose').css('height', '5em');
-		
+
 	});
-	
-	$('.tweet-compose').on('blur', function(){
+
+	$('.tweet-compose').on('blur', function () {
 		$('#tweet-submit').hide();
 		$('#char-count').hide();
 		$('.tweet-compose').css('height', '2.5em');
 	});
-	
-	
-	
-	
-	
-	
-	$('#notReply').keydown(function() {
-				var userTweet = $('#notReply').val().length;
-			if (userTweet <= 140){
-				$('#char-count').text(140 - userTweet);
-				//use a while loop for the userTweet length being 10 or less.
-			if (userTweet >= 130){
+
+
+
+
+
+
+	$('#notReply').keydown(function () {
+		var userTweet = $('#notReply').val().length;
+		if (userTweet <= 140) {
+			$('#char-count').text(140 - userTweet);
+			if (userTweet >= 130) {
 				$('#char-count').css('color', 'red');
 			} else {
 				$('#char-count').css('color', 'black');
 			}
-			return userTweet;
-			
-		
-		} 
+		}
+
+		if (userTweet > 140) {
+			$('#tweet-submit').prop('disabled', true);
+			$('#char-count').text(140 - userTweet);
+		} else {
+			$('#tweet-submit').prop('disabled', false);
+
+		}
+
 	});
-	
+
+
+
 });
